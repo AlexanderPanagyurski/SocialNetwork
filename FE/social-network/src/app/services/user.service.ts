47f439a022b3a8453ea6e6187630ddc759b53539
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2024-07-23 16:16:20 +0300

    add user list component functionality

---
+++ FE/social-network/src/app/services/user.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { User } from '../types/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() {
+    const { apiUrl } = environment;
+
+    var response = this.http.get<User[]>(`${apiUrl}/users`);
+    return response;
+  }
+}
