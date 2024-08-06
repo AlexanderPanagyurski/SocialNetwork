2e4d5ab0ff78d72a64df313c1eb6d9992318a0cb
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2024-08-06 20:45:40 +0300

    update RegisterComponent

--- FE/social-network/src/app/user/register/register.component.ts
+++ FE/social-network/src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, ValidationErrors, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, ValidationErrors, Validators } from '@angular/forms';
 import { EMAIL_DOMAINS } from 'src/app/constants';
 import { emailValidator } from 'src/app/shared/utils/email-validator';
 import { matchPasswordsValidator } from 'src/app/shared/utils/match-passwords-validator';
@@ -15,7 +15,7 @@ export class RegisterComponent {
   form = this.fb.group({
     //controls
     email: ['', [Validators.required, emailValidator(EMAIL_DOMAINS)]],
-    username: ['', [Validators.required, Validators.minLength(6)]],
+    username: ['', [Validators.required, Validators.minLength(4)]],
     passGroup: this.fb.group({
       password: ['', [Validators.required, passwordValidator()]],
       confirmPassword: ['', [Validators.required, passwordValidator()]]
@@ -26,6 +26,18 @@ export class RegisterComponent {
   });
 
   constructor(private fb: FormBuilder) { }
+
+  controlTouched(controlName: string): boolean {
+    return this.form.get(controlName)?.touched || false;
+  }
+
+  controlHasAnyErrors(controlName: string) {
+    return this.form.get(controlName)?.errors || undefined;
+  }
+
+  controlHasErrors(controlName: string, errorType: string) {
+    return this.form.get(controlName)?.errors?.[errorType];
+  }
 
   register(): void {
     if (this.form.invalid) {
