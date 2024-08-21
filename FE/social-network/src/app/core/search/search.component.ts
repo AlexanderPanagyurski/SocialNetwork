2aa0f937a7b3a0a58fe48bf844a1638a1cf785ba
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2024-08-21 15:32:50 +0300

    move seach bar to separate SearchComponent

---
+++ FE/social-network/src/app/core/search/search.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/types/user';
+import { UserService } from 'src/app/user/user.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+  searchedUsers: User[] = [];
+
+  constructor(
+    private userService: UserService,
+    private router: Router) { }
+
+  get searchedUsersCount(): number {
+    return this.searchedUsers.length;
+  }
+
+  searchUser(event: any) {
+    let username = event.target.value;
+    this.userService.getUsersByUsername(username).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.searchedUsers = response;
+      },
+      error: (error) => {
+        console.log('Error: ', error);
+      }
+    })
+  }
+
+  navigateTo(path: string, user?: User) {
+    this.searchedUsers = [];
+
+    if (user) {
+      this.router.navigate([path, user?.userId]);
+    } else {
+      this.router.navigate([path]);
+    }
+  }
+}
+
