c3fdc3dd6b8f41b5cbb20ee41f58db681038a6ae
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2024-09-05 21:59:25 +0300

    implement functionality for adding reply comments

--- FE/social-network/src/app/post/comment-card/comment-card.component.ts
+++ FE/social-network/src/app/post/comment-card/comment-card.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { PostComment } from 'src/app/types/post-comment';
 import { DatePipe } from '@angular/common';
 import { DEFAULT_USER_IMAGE_URL } from 'src/app/constants';
+import { NgForm } from '@angular/forms';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-comment-card',
@@ -9,7 +11,12 @@ import { DEFAULT_USER_IMAGE_URL } from 'src/app/constants';
   styleUrls: ['./comment-card.component.css']
 })
 export class CommentCardComponent {
+  loadReplyCommentTextArea: boolean = false;
+  replyOnId: string | undefined;
+  @Input() postId: string | undefined;
   @Input() postComments: PostComment[] = [] as PostComment[];
+
+  constructor(private postService: PostService) { }
 
   checkIfEdited(postComment: PostComment): string {
     const datePipe: DatePipe = new DatePipe('en-US');
@@ -30,7 +37,32 @@ export class CommentCardComponent {
     return DEFAULT_USER_IMAGE_URL;
   }
 
-  reply(postComment: PostComment) {
-    console.log(postComment);
+  loadReply(postCommentId: string) {
+    console.log(postCommentId);
+    this.loadReplyCommentTextArea = true;
+    this.replyOnId = postCommentId;
+  }
+
+  addComment(form: NgForm, postComment: PostComment) {
+    if (form.invalid) {
+      return;
+    }
+    console.log('Replying on: ', postComment.id);
+
+    const { content } = form.value;
+    debugger;
+    this.postService.addPostComment(this.postId!, postComment.id, content).subscribe({
+      next: (response) => {
+        postComment.children.push(response);
+        let commentsCount = document.getElementById(`post-comments-${this.postId}`);
+        if (commentsCount) {
+          let count = Number(commentsCount.innerHTML)
+          commentsCount.innerHTML = (++count).toString();
+        }
+      },
+      error: (error) => {
+        console.log('Error: ', error);
+      }
+    })
   }
 }
